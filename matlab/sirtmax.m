d0f006edb3b359cfc80f039224de4d920d5e672c
Dimiter Prodanov (imec) <prodanov@imec.be>
2020-12-21 20:31:19 +0100

    code removed

--- matlab/sirtmax.m
+++
@@ -1,7 +0,0 @@
-function tm=sirtmax(g, s0, i0)
-   warning off;
-   tol=1e-15;
-   fun = @(u) 1./( -(s0+i0) + s0*exp(u) -g*u );
-   tm=integral(fun, 0, log(g/s0), 'RelTol',tol,'AbsTol',1e-8);
-end
-  
